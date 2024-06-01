e2a0b969df86c6dd01c26f828d9f60945149b71f
RdrigoFH <rfernandezh@unsa.edu.pe>
2024-06-01 12:57:46 -0500

    Se crea el modelo contacto

--- personas/models.py
+++ personas/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Contacto(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.nombre
